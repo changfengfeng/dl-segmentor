11ad95cde123682b035df2baf2aecf8360f25b78
changfengfeng <changfengfeng@ling.ai>
2017-09-30 14:30:46 +0800

    9.30 version

--- src/segmentor.py
+++ src/segmentor.py
@@ -1,13 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import tensorflow as tf
 import numpy as np
 import word2vec as w2v
 import time
+
+FLAGS = tf.flags.FLAGS
+tf.flags.DEFINE_bool("using_lstm", False, "using lstm or id cnn model")
+tf.flags.DEFINE_bool("testing", False, "test accuracy")
 
 class Segmentor:
     """ Import lexicon and model, segment text
     """
 
     def __init__(self, lexicon_path, model_path, model_prefix, max_seq_length):
+
+        self.pair_marks = {"(": ")",
+                "（" : "）",
+                "["  : "]",
+                "【" : "】",
+                "《" : "》",
+                '“'  : '”'}
+
+        self.break_marks = set(["。", ",", "，", " ", "\t", "?", "？", "!",
+            "！", ";", "；"])
+        self.pair_marks_reverse = {v:k for k, v in self.pair_marks.items()}
 
         self.max_seq_length = max_seq_length
         self.lexicon_path = lexicon_path
@@ -17,7 +34,7 @@ class Segmentor:
         # load lexicon
         model = w2v.load(lexicon_path)
         self.lexicon = {w:i for i, w in enumerate(model.vocab)}
-        self.vocab = model.vocab
+        self.unk = self.lexicon['<UNK>']
 
         # load model
         with tf.gfile.GFile(model_path, "rb") as f:
@@ -29,14 +46,14 @@ class Segmentor:
 
         graph = tf.get_default_graph()
         self.x_inputs = graph.get_tensor_by_name("%s/test_input_x:0" % model_prefix)
-        self.keep_rate = graph.get_tensor_by_name("%s/lstm/keep_rate:0" % model_prefix)
+        if FLAGS.using_lstm:
+            self.keep_rate = graph.get_tensor_by_name("%s/lstm/keep_rate:0" % model_prefix)
+        else:
+            self.keep_rate = graph.get_tensor_by_name("%s/id_cnn/keep_rate:0" % model_prefix)
         self.logits = graph.get_tensor_by_name("%s/logits_crf:0" % model_prefix)
         self.transition_params = graph.get_tensor_by_name("%s/transitions:0" %
                 model_prefix)
         self.sess = tf.Session()
-
-        print(self.transition_params)
-        print(self.logits)
 
     def test_accuracy(self, validate_data_fn):
         x_inputs = []
@@ -78,6 +95,31 @@ class Segmentor:
 
         print("validate accuray: {:.4f}".format(correct_label / total_label))
 
+    def split_text(self, sentence):
+        """ split sentence to sentences by colon"""
+        results = []
+        start = 0
+        # state = 0 in pair marks, state = 1 not in pair makrs
+        state = 0
+        for idx in range(len(sentence)):
+            char = sentence[idx]
+            if char in self.pair_marks:
+                state = 1
+                if start < idx: # for case [][]
+                    results.append(sentence[start:idx])
+                start = idx
+            elif char in self.pair_marks_reverse and state == 1:
+                if self.pair_marks_reverse[char] == sentence[start]:
+                    state = 0
+                    results.append(sentence[start:idx+1])
+                    start = idx + 1
+            elif char in self.break_marks:
+                results.append(sentence[start:idx+1])
+                start = idx + 1
+        if start < len(sentence):
+            results.append(sentence[start:])
+        return results
+
     def segment(self, sentence):
         """segment sentence
 
@@ -86,48 +128,66 @@ class Segmentor:
         Returns:
             list of string
         """
-        inputs = []
-        for s in sentence:
-            if s in [" ", "\t", "\n", "\r"]:
-                continue
-            sid = self.lexicon[s] if s in self.lexicon else self.lexicon['<UNK>']
-            inputs.append(sid)
-        real_length = len(inputs)
-        for i in range(len(inputs), 80):
-            inputs.append(0)
+        split_sentences = self.split_text(sentence)
+        x_inputs_val = np.zeros([len(split_sentences), self.max_seq_length], dtype="int32")
+        for i in range(len(split_sentences)):
+            for j in range(len(split_sentences[i])):
+                s = split_sentences[i][j]
+                sid = self.lexicon[s] if s in self.lexicon else self.unk
+                x_inputs_val[i,j] = sid
 
-        x_inputs_val = [inputs]
+        print(split_sentences)
+        print(x_inputs_val)
+
+        real_lengths = np.sum(np.sign(x_inputs_val), axis=1)
+
         start = time.time()
-        logits_val, keep_rate_val, transitions = self.sess.run(
-                [self.logits, self.keep_rate, self.transition_params],
+        logits_val, transitions = self.sess.run(
+                [self.logits, self.transition_params],
                 {self.x_inputs: x_inputs_val, self.keep_rate: 1.0})
-        real_logit = logits_val[0][:real_length]
-        decoded_seq, _ = tf.contrib.crf.viterbi_decode(real_logit, transitions)
+
+        # TODO using user defined dict
+        decoded_results = []
+        for logit, real_length in zip(logits_val, real_lengths):
+            real_logit = logit[:real_length]
+            decoded_seq, _ = tf.contrib.crf.viterbi_decode(real_logit, transitions)
+            decoded_results.append(decoded_seq)
         end = time.time()
         print("time: {:.4f}".format(end-start))
-        # decode crf
-        output = []
-        word = ""
-        for char_id, mark in zip(inputs, decoded_seq):
-            char = self.vocab[char_id]
-            if mark == 0:
-                word = char
-                output.append(word)
-            if mark == 1:
-                word = char
-            if mark == 2:
-                word += char
-            if mark == 3:
-                word += char
-                output.append(word)
 
-        print("\n".join(output))
+        # decode crf
+        results = []
+        for sentence, tags in zip(split_sentences, decoded_results):
+            assert len(sentence) == len(tags)
+            word = ""
+            for s, t in zip(sentence, tags):
+                if t == 0:
+                    results.append(s)
+                if t == 1:
+                    word += s
+                if t == 2:
+                    word += s
+                if t == 3:
+                    word += s
+                    results.append(word)
+                    word = ""
+
+        for s in results:
+            print("tok:", s)
+
+def main(_):
+    if FLAGS.using_lstm:
+        model_fn = "model/segment_model.pbtxt"
+    else:
+        model_fn = "model/segment_model_idcnn.pbtxt"
+
+    segmentor = Segmentor("model/char_pepole_vec.txt", model_fn, "segment", 80)
+    if FLAGS.testing:
+        segmentor.test_accuracy("model/test.txt")
+    else:
+        while True:
+            sentence = input(">")
+            segmentor.segment(sentence)
 
 if __name__ == "__main__":
-    segmentor = Segmentor("data/char_pepole_vec.txt",
-    "model/segment_model.pbtxt", "segment", 80)
-    #segmentor.test_accuracy("model/test.txt")
-    while True:
-        sentence = input(">")
-        segmentor.segment(sentence)
-
+    tf.app.run()
--- src/train_segmentor_queue.py
+++ src/train_segmentor_queue.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
 import numpy as np
 import word2vec as w2v
